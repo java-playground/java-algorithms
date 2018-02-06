d506681c2f8e57c2f6d5eb5a29f9cbf58aa5731d
Jonathan Lam <jlam55555@gmail.com>
2018-02-06 00:41:41 -0500

    commented up factorial class

--- src/Factorial.java
+++ src/Factorial.java
@@ -18,5 +18,30 @@ public class Factorial {
 
         System.out.println(factorial);
 
+
+        /**
+         *
+         * WALKTHROUGH
+         *
+         * Set the number to take the factorial of
+         * int n = 1000;
+         *
+         * Essentially create an array from [1, 2, 3, ... n]
+         * IntStream.rangeClosed(1, n)
+         *
+         * Go through every element of the array you just created and turn it into a BigInteger
+         * .mapToObj(m -> BigInteger.valueOf(m))
+         *
+         * Multiply all of the terms into one product using BigInteger's .multiply() method
+         * Here BigInteger.valueOf(1) is the initial accumulator value
+         * a is the accumulator
+         * b is the next element of the array
+         * .reduce(BigInteger.valueOf(1), (a, b) -> a.multiply(b))
+         *
+         * This is obvious
+         * .toString()
+         *
+         */
+
     }
 }
